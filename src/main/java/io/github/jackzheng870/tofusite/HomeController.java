63bc5f54c3dac425f74bdf97cc699ebe95ab0802
Jack Zheng <jackzheng870@icloud.com>
2021-12-08 01:34:52 +0800

    改用 MCProtocolLib 取代在线 API

--- src/main/java/io/github/jackzheng870/tofusite/HomeController.java
+++ src/main/java/io/github/jackzheng870/tofusite/HomeController.java
@@ -1,12 +1,14 @@
 package io.github.jackzheng870.tofusite;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.github.steveice10.mc.protocol.MinecraftConstants;
+import com.github.steveice10.mc.protocol.MinecraftProtocol;
+import com.github.steveice10.mc.protocol.data.status.handler.ServerPingTimeHandler;
+import com.github.steveice10.packetlib.tcp.TcpClientSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,52 +16,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    private String name;
-    private String server;
-
-    private HttpClient client = HttpClient.newHttpClient();
-    private HttpRequest mcsrvstatusRequest;
-    private HttpRequest mcapiRequest;
-
-    private boolean mcsrvstatusOnline;
-    private boolean mcapiOnline;
+    private TofusiteProperties properties;
 
     public HomeController(TofusiteProperties properties) {
-        name = properties.getName();
-        server = properties.getServer();
-
-        mcsrvstatusRequest = buildRequest("https://api.mcsrvstat.us/2/" + server);
-        mcapiRequest = buildRequest("https://mcapi.us/server/status?ip=" + server);
+        this.properties = properties;
     }
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("name", name);
-        model.addAttribute("server", server);
-
-        CompletableFuture.allOf(sendRequest(mcsrvstatusRequest).thenAccept(json -> {
-            try {
-                mcsrvstatusOnline = us.mcsrvstat.api.Converter.fromJsonString(json).getOnline();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }), sendRequest(mcapiRequest).thenAccept(json -> {
-            try {
-                mcapiOnline = us.mcapi.Converter.fromJsonString(json).getOnline();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        })).join();
-
-        model.addAttribute("online", mcsrvstatusOnline || mcapiOnline);
+        model.addAttribute("name", properties.getName());
+        model.addAttribute("host", properties.getHost());
+        model.addAttribute("online", isOnline());
         return "home";
     }
 
-    private HttpRequest buildRequest(String uri) {
-        return HttpRequest.newBuilder().uri(URI.create(uri)).build();
-    }
-
-    private CompletableFuture<String> sendRequest(HttpRequest request) {
-        return client.sendAsync(request, BodyHandlers.ofString()).thenApply(HttpResponse::body);
+    private boolean isOnline() {
+        CompletableFuture<Boolean> future = new CompletableFuture<>();
+        TcpClientSession client = new TcpClientSession(properties.getHost(), properties.getPort(),
+                new MinecraftProtocol());
+        client.setFlag(MinecraftConstants.SERVER_PING_TIME_HANDLER_KEY,
+                (ServerPingTimeHandler) (session, ping) -> future.complete(true));
+        client.connect();
+        try {
+            return future.get(properties.getTimeout(), TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
--- src/main/java/io/github/jackzheng870/tofusite/TofusiteProperties.java
+++ src/main/java/io/github/jackzheng870/tofusite/TofusiteProperties.java
@@ -3,25 +3,14 @@ package io.github.jackzheng870.tofusite;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import lombok.Data;
+
 @ConfigurationProperties("tofusite")
 @Component
+@Data
 public class TofusiteProperties {
     private String name = "豆服";
-    private String server = "server.tofuhero.me";
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getServer() {
-        return server;
-    }
-
-    public void setServer(String server) {
-        this.server = server;
-    }
+    private String host = "server.tofuhero.me";
+    private int port = 25565;
+    private int timeout = 10;
 }
